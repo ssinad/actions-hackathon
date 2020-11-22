97b360b8375f2176e2c7f5f7a08a07837db0927e
ssinad <27953063+ssinad@users.noreply.github.com>
2020-11-22 10:56:07 -0700

    Add new workflow

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Decrypt the file
 mkdir $GITHUB_WORKSPACE/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_FILE_PASSPHRASE" \
 --output $GITHUB_WORKSPACE/secrets/my_secret.json my_secret.json.gpg
